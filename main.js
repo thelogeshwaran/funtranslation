1302759b321b507fc8a8a2fe351659edd4bc28f8
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2020-12-05 01:30:00 +0530

    doc : update main.js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var input= document.querySelector("#inputTxt");
 var buttonTrans = document.querySelector("#button");
 var output = document.querySelector("#outputTxt");
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+/** var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" **/
+
+var serverUrl="https://api.funtranslations.com/translate/navi.json"
 
 
 function getUrl(txtFrurl){
@@ -19,10 +21,15 @@ function clickHandler(){
     .then(json => { 
         output.innerText = json.contents.translated;
     })
+    .catch(errorHandler)
     
 
 
 }
 
+function errorHandler(error){
+    console.log(error)
+    alert("Something wrong in the server .Please try again")
+}
 
 buttonTrans.addEventListener("click", clickHandler)
