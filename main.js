daa94ee4b72874d1b6cc96c971156a5839a0eb95
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2020-12-05 01:13:47 +0530

    feat : clickHanlder add

---
+++ main.js
@@ -0,0 +1,14 @@
+var input= document.querySelector("#inputTxt");
+var buttonTrans = document.querySelector("#button");
+var output = document.querySelector("#outputTxt");
+
+
+
+function clickHandler(){
+    console.log(input.value)
+
+
+}
+
+
+buttonTrans.addEventListener("click", clickHandler)
