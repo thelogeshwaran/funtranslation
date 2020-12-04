88f06d756de44588235ba182cb16b654b577e7e5
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2020-12-05 01:19:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ var input= document.querySelector("#inputTxt");
 var buttonTrans = document.querySelector("#button");
 var output = document.querySelector("#outputTxt");
 
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+function getUrl(txtFrurl){
+    return serverUrl + "?" + "text=" + txtFrurl
+}
 
 
 function clickHandler(){
-    console.log(input.value)
+
+    var inputval = input.value
+
+    fetch(getUrl(inputval))
+    .then(response => response.json())
+    .then(json => { 
+        output.innerText = json.contents.translated;
+    })
+    
 
 
 }
