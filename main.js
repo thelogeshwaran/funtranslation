76a287682e2c96188b8d16683442320f60f6a1b7
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2020-12-05 12:56:46 +0530

    update main.css

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var input= document.querySelector("#inputTxt");
 var buttonTrans = document.querySelector("#button");
 var output = document.querySelector("#outputTxt");
 
-/** var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" **/
+/**  var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"  **/
 
-var serverUrl="https://api.funtranslations.com/translate/navi.json"
+ var serverUrl="https://api.funtranslations.com/translate/navi.json" 
 
 
 function getUrl(txtFrurl){
